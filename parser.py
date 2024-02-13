45bc79bf95b1cfe8406cee34606a3dd9c9755aba
GordonZhang2024 <159539185+GordonZhang2024@users.noreply.github.com>
2024-02-13 21:08:07 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,8 +3,7 @@ with open('sample.md') as sample:
     lines = sample.readlines()
 
 for index, line in enumerate(lines):
-    
-    head = re.match(r'#* ', line)
+    head = re.match(r'[^\\]#+ ', line)
     if head != None:
         head = str(head.group(0))
         len = len(head) - 1
@@ -13,4 +12,9 @@ for index, line in enumerate(lines):
             line = line + f'</h{len}>'
             lines[index] = line
 
+    bold = re.search(r'\*\*\w+\*\*\W', line)
+        if bold != None:
+            bold = str(bold.group)
+        
+            print('bold: ', bold)
 print(lines)
