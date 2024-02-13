833c88641f8f3edfbe423dfee499fdbfa385d977
GordonZhang2024 <159539185+GordonZhang2024@users.noreply.github.com>
2024-02-13 20:35:45 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,19 +1,16 @@
-
 import re
 with open('sample.md') as sample:
     lines = sample.readlines()
 
 for index, line in enumerate(lines):
     
-    head = re.match(r'[^\\]#', line)
+    head = re.match(r'#* ', line)
     if head != None:
         head = str(head.group(0))
-        len = len(head)
+        len = len(head) - 1
         if len <= 6:
             line = line.replace(head, f'<h{len}>')
             line = line + f'</h{len}>'
             lines[index] = line
-
-
 
 print(lines)
