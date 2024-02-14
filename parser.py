4ff5bf301c05189504573b68037b9d2b7c0a2d6e
GordonZhang2024 <159539185+GordonZhang2024@users.noreply.github.com>
2024-02-14 08:56:16 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,16 +6,16 @@ for index, line in enumerate(lines):
     head = re.match(r'[^\\]#+ ', line)
     if head != None:
         head = str(head.group(0))
-        len = len(head) - 1
-        if len <= 6:
-            line = line.replace(head, f'<h{len}>')
-            line = line + f'</h{len}>'
+        lenth = len(head) - 1
+        if lenth <= 6:
+            line = line.replace(head, f'<h{lenth}>')
+            line = line + f'</h{lenth}>'
             lines[index] = line
 
-    bold = re.search(r'[\*_]{2}\w+[\*_]{2}', line)
+    bold = re.findall(r'[\*_]{2}\w+[\*_]{2}', line)
         if bold != None:
-            for i in bold.groups:
-                strong = '<strong>' + bold[2:-2] + </strong>
+            for i in bold:
+                strong = '<strong>' + bold[2:-2] + '</strong>'
                 line.replace(i, strong)
             
 print(lines)
