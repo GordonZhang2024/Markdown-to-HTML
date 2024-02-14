b2a9a010181600da65f331e0b8159977bb7121f3
GordonZhang2024 <159539185+GordonZhang2024@users.noreply.github.com>
2024-02-14 21:03:06 +0800

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,21 +1,30 @@
 import re
 with open('sample.md') as sample:
-    lines = sample.readlines()
+    lines = sample.read()
+    lines = lines.splitlines()
 
 for index, line in enumerate(lines):
-    head = re.match(r'[^\\]#+ ', line)
+    head = re.match(r'#+ ', line)
     if head != None:
         head = str(head.group(0))
         lenth = len(head) - 1
         if lenth <= 6:
             line = line.replace(head, f'<h{lenth}>')
             line = line + f'</h{lenth}>'
-            lines[index] = line
 
-    bold = re.findall(r'[\*_]{2}\w+[\*_]{2}', line)
-        if bold != None:
-            for i in bold:
-                strong = '<strong>' + bold[2:-2] + '</strong>'
-                line.replace(i, strong)
-            
+    bold = re.findall(r'[\*_]{2}[\w\s]+?[\*_]{2}', line)
+    if bold != None:
+        for i in bold:
+            strong = '<strong>' + i[2:-2] + '</strong>'
+            line = line.replace(i, strong)
+
+    italic = re.findall(r'[\*_][\w\s]+?[\*_]', line)
+    if italic != None:
+        print(italic)
+        for i in italic:
+            em = '<em>' + i[1:-1] + '</em>'
+            line = line.replace(i, em)
+
+
+    lines[index] = line
 print(lines)
