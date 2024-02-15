63a253be5e47450b790df1921eb86af2a61c1fd0
GordonZhang2024 <159539185+GordonZhang2024@users.noreply.github.com>
2024-02-15 09:50:59 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,30 +1,32 @@
 import re
-with open('sample.md') as sample:
-    lines = sample.read()
-    lines = lines.splitlines()
 
-for index, line in enumerate(lines):
-    head = re.match(r'#+ ', line)
-    if head != None:
-        head = str(head.group(0))
-        lenth = len(head) - 1
-        if lenth <= 6:
-            line = line.replace(head, f'<h{lenth}>')
-            line = line + f'</h{lenth}>'
+def parser(md: list):
+    '''
+    md
+    '''
+    for index, line in enumerate(md):
+    
+        h_ = re.match(r'#+ ', line)
+        if h_ != None:
+            h_ = str(h_.group(0))
+            lenth = len(h_) - 1
+            if lenth <= 6:
+                line = line.replace(h_, f'<h{lenth}>')
+                line = line + f'</h{lenth}>'
 
-    bold = re.findall(r'[\*_]{2}[\w\s]+?[\*_]{2}', line)
-    if bold != None:
-        for i in bold:
-            strong = '<strong>' + i[2:-2] + '</strong>'
-            line = line.replace(i, strong)
+        bold = re.findall(r'[\*_]{2}[\w\s]+?[\*_]{2}', line)
+        if bold != None:
+            for i in bold:
+                strong = '<strong>' + i[2:-2] + '</strong>'
+                line = line.replace(i, strong)
 
-    italic = re.findall(r'[\*_][\w\s]+?[\*_]', line)
-    if italic != None:
-        print(italic)
-        for i in italic:
-            em = '<em>' + i[1:-1] + '</em>'
-            line = line.replace(i, em)
+        italic = re.findall(r'[\*_][\w\s]+?[\*_]', line)
+        if italic != None:
+            print(italic)
+            for i in italic:
+                em = '<em>' + i[1:-1] + '</em>'
+                line = line.replace(i, em)
 
 
-    lines[index] = line
+        md[index] = line
 print(lines)
